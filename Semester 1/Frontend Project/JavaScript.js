1d4c8aef28c6cab0c6273d2361971bc9b736ba29
Ayush Sharma <ashrma0502@gmail.com>
2025-11-27 10:12:40 +0530

    Frontend project

--- Semester 1/Frontend Project/JavaScript.js
+++ Semester 1/Frontend Project/JavaScript.js
@@ -1,6 +1,7 @@
 let question = [];
 let i = 0;
 let answers = [];
+// Objective: To fetch 15 random questions from a json containing 50 qustions and store then in questions array
 async function questions() {
     const res = await fetch("Project.json");
     const data = await res.json();
@@ -21,6 +22,7 @@ async function questions() {
     }
     display();
 }
+// Objective: Function used to display questions
 function display() {
     let q = question[i];
     document.getElementById("question").innerHTML = "<h4>Q" + (i + 1) + ". " + q.question + "</h4>";
@@ -34,6 +36,7 @@ function display() {
         document.getElementById("option").innerHTML = "<div class='p-3'>" + "<label class='option-block'><input type='radio' name='opt' value='0' " + (answers[i] == 0 ? "checked" : "") + "> " + q.options[0] + "</label><hr>" + "<label class='option-block'><input type='radio' name='opt' value='1' " + (answers[i] == 1 ? "checked" : "") + "> " + q.options[1] + "</label><hr>" + "<label class='option-block'><input type='radio' name='opt' value='2' " + (answers[i] == 2 ? "checked" : "") + "> " + q.options[2] + "</label><hr>" + "<label class='option-block'><input type='radio' name='opt' value='3' " + (answers[i] == 3 ? "checked" : "") + "> " + q.options[3] + "</label>" + "</div><br>" + "<button class='btn btn-primary float-start' onclick='previous()'>Previous</button>" + "<button class='btn btn-primary float-end' onclick='submit()'>Submit</button>";
     }
 }
+// Objective: Function used to display the next question
 function next() {
     let s = document.querySelector("input[name='opt']:checked");
     if (s) {
@@ -47,6 +50,7 @@ function next() {
         display();
     }
 }
+// Objective: Function used to display the previous question 
 function previous() {
     let s = document.querySelector("input[name='opt']:checked");
     if (s) {
@@ -61,6 +65,7 @@ function previous() {
     }
 }
 let total = 0;
+// Objective: Function used to show the result page when submit button is clicked
 function submit() {
     let s = document.querySelector("input[name='opt']:checked");
     if (s) {
@@ -79,6 +84,7 @@ function submit() {
     localStorage.setItem("questions", JSON.stringify(question));
     window.location.href = "Result.html";
 }
+// Objective: Function to show the result of the test and show the correct answers for the questions in which the asnwers were wrong 
 function result() {
     let total = localStorage.getItem("score");
     let answers = JSON.parse(localStorage.getItem("answers"));
@@ -106,6 +112,7 @@ function result() {
     }
     document.getElementById("review").innerHTML = output;
 }
+// Objective: Function used to change the theme of page to light or darks as per need
 function theme() {
     let select = document.getElementById("theme-select").value;
     let body = document.body;
@@ -128,6 +135,7 @@ function theme() {
 
     localStorage.setItem("theme", select);
 }
+// Objective: Function used to know the theme of the page selected by the user
 function init() {
     let saved = localStorage.getItem("theme") || "light";
     const select = document.getElementById("theme-select");
